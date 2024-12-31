b076618227ac4672e4694882490adc33a0321538
IzeAack <114884311+IzeAack@users.noreply.github.com>
2024-12-31 18:18:56 -0500

    Add files via upload

---
+++ TwitchPlays_Settings.py
@@ -0,0 +1,10 @@
+# Replace this with your Twitch username. Must be all lowercase.
+TWITCH_CHANNEL = 'twitch_channel_here' 
+
+# If you're streaming on Youtube, replace this with your Youtube's Channel ID
+# Find this by clicking your Youtube profile pic -> Settings -> Advanced Settings
+YOUTUBE_CHANNEL_ID = "youtube_channel_id_here" 
+
+# If you're using an Unlisted stream to test on Youtube, replace "None" below with your stream's URL in quotes.
+# Otherwise you can leave this as "None"
+YOUTUBE_STREAM_URL = None
