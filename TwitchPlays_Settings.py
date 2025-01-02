426493c54eb5d192206fa6e114ccc1ccd1fd20a7
IzeAack <114884311+IzeAack@users.noreply.github.com>
2025-01-02 16:28:25 -0500

    Update TwitchPlays_Settings.py

--- TwitchPlays_Settings.py
+++ TwitchPlays_Settings.py
@@ -1,5 +1,14 @@
+##################### GAME VARIABLES #####################
+
 # Replace this with your Twitch username. Must be all lowercase.
 TWITCH_CHANNEL = 'twitch_channel_here' 
+
+# Please enable one of the streaming options
+STREAMING_ON_BOTH = False
+
+STREAMING_ON_TWITCH = False
+
+STREAMING_ON_YOUTUBE = False
 
 # If you're streaming on Youtube, replace this with your Youtube's Channel ID
 # Find this by clicking your Youtube profile pic -> Settings -> Advanced Settings
@@ -8,3 +17,20 @@ YOUTUBE_CHANNEL_ID = "youtube_channel_id_here"
 # If you're using an Unlisted stream to test on Youtube, replace "None" below with your stream's URL in quotes.
 # Otherwise you can leave this as "None"
 YOUTUBE_STREAM_URL = None
+
+##################### MESSAGE QUEUE VARIABLES #####################
+
+# MESSAGE_RATE controls how fast we process incoming Twitch Chat messages. It's the number of seconds it will take to handle all messages in the queue.
+# This is used because Twitch delivers messages in "batches", rather than one at a time. So we process the messages over MESSAGE_RATE duration, rather than processing the entire batch at once.
+# A smaller number means we go through the message queue faster, but we will run out of messages faster and activity might "stagnate" while waiting for a new batch. 
+# A higher number means we go through the queue slower, and messages are more evenly spread out, but delay from the viewers' perspective is higher.
+# You can set this to 0 to disable the queue and handle all messages immediately. However, then the wait before another "batch" of messages is more noticeable.
+MESSAGE_RATE = 0.5
+# MAX_QUEUE_LENGTH limits the number of commands that will be processed in a given "batch" of messages. 
+# e.g. if you get a batch of 50 messages, you can choose to only process the first 10 of them and ignore the others.
+# This is helpful for games where too many inputs at once can actually hinder the gameplay.
+# Setting to ~50 is good for total chaos, ~5-10 is good for 2D platformers
+MAX_QUEUE_LENGTH = 20
+MAX_WORKERS = 100 # Maximum number of threads you can process at a time 
+
+##########################################################
